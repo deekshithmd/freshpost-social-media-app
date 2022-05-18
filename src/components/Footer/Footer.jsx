caa76ada1608a52cbcf2d52f84a7d81e7ace8e4f
Deekshith M D <deekshithmogra@gmail.com>
2022-05-18 09:24:59 +0530

    refactor: implemented review changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,43 +1,41 @@
 import "./footer.css";
 export const Footer = () => {
+  const personalData = [
+    {
+      link: "https://github.com/deekshithmd",
+      icon: "fa-github",
+    },
+    {
+      link: "https://in.linkedin.com/in/deekshith-m-d-42a306154",
+      icon: "fa-linkedin",
+    },
+    {
+      link: "http://twitter.com/deekshith_md",
+      icon: "fa-twitter",
+    },
+  ];
   return (
     <footer className="footer">
       <span className="text-md footer-text text-center">
         {" "}
-        <span className="brand-style">FreshPost</span> developed and maintained by
-        Deekshith M D
+        <span className="brand-style">FreshPost</span> developed and maintained
+        by Deekshith M D
       </span>
       <ul className="footer-social-icons">
-        <li className="list-inline-item">
-          <a
-            href="https://github.com/deekshithmd"
-            target="_blank"
-            rel="noreferrer"
-            className="nav-icon-link link-style-none"
-          >
-            <i className="fab fa-github nav-icon"></i>{" "}
-          </a>
-        </li>
-        <li className="list-inline-item">
-          <a
-            href="https://in.linkedin.com/in/deekshith-m-d-42a306154"
-            target="_blank"
-            rel="noreferrer"
-            className="nav-icon-link link-style-none"
-          >
-            <i className="fab fa-linkedin nav-icon"></i>
-          </a>
-        </li>
-        <li className="list-inline-item">
-          <a
-            href="http://twitter.com/deekshith_md"
-            target=" _blank"
-            rel="noreferrer"
-            className="nav-icon-link link-style-none"
-          >
-            <i className="fab fa-twitter nav-icon"></i>
-          </a>
-        </li>
+        {personalData.map((data) => {
+          return (
+            <li className="list-inline-item">
+              <a
+                href={data.link}
+                target="_blank"
+                rel="noreferrer"
+                className="nav-icon-link link-style-none"
+              >
+                <i className={`fab ${data.icon} nav-icon`}></i>{" "}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </footer>
   );
