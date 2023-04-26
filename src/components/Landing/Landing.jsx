22820d7db393beb74c1502829b6ab338dce8c38c
deekshithmd <deekshithmogra@gmail.com>
2023-04-26 22:33:12 +0530

    updated routes

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,11 +3,6 @@ import { NavLink, Outlet } from "react-router-dom";
 import image from "assets/svg1.svg";
 
 export const Landing = () => {
-  const activeStyle = {
-    backgroundColor: "var(--primary-color)",
-    width: "50%",
-  };
-
   return (
     <div className="landing-container">
       <div className="home-image flex">
@@ -16,16 +11,20 @@ export const Landing = () => {
       <div className="authentication-section">
         <div className="options">
           <NavLink
-            className="item-1 flex link-style-none text-lg text-bold"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            to="login"
+            className={`${({ isActive }) =>
+              isActive
+                ? "selected"
+                : ""} item-1 flex link-style-none text-lg text-bold`}
+            to="/login"
           >
             Login
           </NavLink>
           <NavLink
-            className="item-2 flex link-style-none text-lg text-bold"
+            className={`${({ isActive }) =>
+              isActive
+                ? "selected"
+                : ""} item-2 flex link-style-none text-lg text-bold`}
             to="signup"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
           >
             Register
           </NavLink>
